f86e35393c820e40ae6a25f3e150ce0c937c040e
sushant pandey <pandeysushant@eng.rizvi.edu.in>
2026-04-19 15:50:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// Static folders
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "images")));
+
+// Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
+
+app.get("/skill", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "skills.html"));
+});
+
+// Start server
+app.listen(port, () => {
+    console.log("Server started on http://localhost:" + port);
+});
